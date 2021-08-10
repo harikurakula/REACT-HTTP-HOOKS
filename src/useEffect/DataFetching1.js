415796cda18accb605a4d5dd254832d76785f1f8
Hari Kurakula <harikurakula143@gmail.com>
2021-08-10 11:58:59 +0530

    commit -m ("Enter")

--- src/useEffect/DataFetching1.js
+++ src/useEffect/DataFetching1.js
@@ -4,9 +4,10 @@ import axios from "axios"
 function DataFetching1() {
   const [post, setPost] = useState({})
   const [id, setId] = useState(1)
+  const [idFromButtonClick, setIdFromButtonClick] = useState(1)
   useEffect(() => {
     axios
-      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
       .then((res) => {
         console.log(res)
         setPost(res.data)
@@ -14,10 +15,16 @@ function DataFetching1() {
       .catch((err) => {
         console.log(err)
       })
-  }, [id])
+  }, [idFromButtonClick])
+  const handleClick = () => {
+    setIdFromButtonClick(id)
+  }
   return (
     <div>
       <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+      <button type="button" onClick={handleClick}>
+        Fetch Post
+      </button>
       <div>{post.title}</div>
       {/* //   <ul>
     //     {posts.map((post) => (
