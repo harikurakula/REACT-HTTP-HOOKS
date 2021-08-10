7eee9c8672d56eda8eb8ad9fa65c7e13b680ed03
Hari Kurakula <harikurakula143@gmail.com>
2021-08-10 17:18:59 +0530

    commit -m ("Enter")

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css"
+import React from "react"
+import ComponentC from "./useContext/ComponentC"
 // import DataFetching from "./useEffect/DataFetching"
-import DataFetching1 from "./useEffect/DataFetching1"
+// import DataFetching1 from "./useEffect/DataFetching1"
 //import HookCounterFour from "./Hooks/HookCounterFour"
 // import IntervalClassCounter from "./useEffect/IntervalClassCounter"
 // import IntervalHookCounter from "./useEffect/IntervalHookCounter"
@@ -10,7 +12,8 @@ import DataFetching1 from "./useEffect/DataFetching1"
 // import HookCounterTwo from "./Hooks/HookCounterTwo"
 // import PostForm from "./components/PostForm"
 // import PostList from "./components/PostList"
-
+export const UserContext = React.createContext()
+export const ChannelContext = React.createContext()
 function App() {
   return (
     <div className="App">
@@ -24,7 +27,12 @@ function App() {
       {/* <IntervalClassCounter /> */}
       {/* <IntervalHookCounter /> */}
       {/* <DataFetching /> */}
-      <DataFetching1 />
+      {/* <DataFetching1 /> */}
+      <UserContext.Provider value={"hari"}>
+        <ChannelContext.Provider value={"vijayawada"}>
+          <ComponentC />
+        </ChannelContext.Provider>
+      </UserContext.Provider>
     </div>
   )
 }
