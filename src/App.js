4b08ebb258e64bdc5479cfe231bc038340508865
Hari Kurakula <harikurakula143@gmail.com>
2021-08-11 14:46:00 +0530

    commit -m

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-import "./App.css"
 import React from "react"
-import ComponentC from "./useContext/ComponentC"
+import "./App.css"
+import DataFetchingOne from "./useReduce/DataFetchingOne"
+//import React, { useReducer } from "react"
+// import ComponentA from "./useReduce/ComponentA"
+// import ComponentB from "./useReduce/ComponentB"
+// import ComponentC from "./useReduce/ComponentC"
+//export const CountContext = React.createContext()
+// import ComponentC from "./useContext/ComponentC"
 // import DataFetching from "./useEffect/DataFetching"
 // import DataFetching1 from "./useEffect/DataFetching1"
 //import HookCounterFour from "./Hooks/HookCounterFour"
@@ -12,11 +18,30 @@ import ComponentC from "./useContext/ComponentC"
 // import HookCounterTwo from "./Hooks/HookCounterTwo"
 // import PostForm from "./components/PostForm"
 // import PostList from "./components/PostList"
-export const UserContext = React.createContext()
-export const ChannelContext = React.createContext()
+//export const UserContext = React.createContext()
+//export const ChannelContext = React.createContext()
+// const initialState = 0
+// const reducer = (state, action) => {
+//   switch (action) {
+//     case "increment":
+//       return state + 1
+//     case "decrement":
+//       return state - 1
+//     case "reset":
+//       return initialState
+//     default:
+//       return state
+//   }
+// }
+
 function App() {
+  // const [count, dispatch] = useReducer(reducer, initialState)
   return (
+    // <CountContext.Provider
+    //   value={{ countState: count, countDispatch: dispatch }}
+    // >
     <div className="App">
+      {/* Count - {count} */}
       {/* <PostList /> */}
       {/* <PostForm /> */}
       {/* <ClassCounter /> */}
@@ -28,12 +53,17 @@ function App() {
       {/* <IntervalHookCounter /> */}
       {/* <DataFetching /> */}
       {/* <DataFetching1 /> */}
-      <UserContext.Provider value={"hari"}>
-        <ChannelContext.Provider value={"vijayawada"}>
-          <ComponentC />
-        </ChannelContext.Provider>
-      </UserContext.Provider>
+      {/* <UserContext.Provider value={"hari"}> */}
+      {/* <ChannelContext.Provider value={"vijayawada"}> */}
+      {/* <ComponentC /> */}
+      {/* </ChannelContext.Provider> */}
+      {/* </UserContext.Provider> */}
+      {/* <ComponentA />
+        <ComponentB />
+        <ComponentC /> */}
+      <DataFetchingOne />
     </div>
+    // </CountContext.Provider>
   )
 }
 
